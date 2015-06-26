0191e1b9c7cb70e9b8df8c3a84b081b982747974
Dani Fdez <daniblups@gmail.com>
2015-06-26 21:54:21 +0200

    Modificamos rutas de app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: 'visitors#index'
+  root to: 'home#index'
   devise_for :users
   resources :users
+  
+  resources :comparative_products
+  resources :products
+  resources :comparatives
+  resources :clients
+  
+  # get "/pdf" => "pdfs#ejemplo"
 end
